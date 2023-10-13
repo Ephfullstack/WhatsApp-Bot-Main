df7a137b4af49ff5cd703e5da7e13709ffffb34a
Ephron Agyeman-Opoku <94077473+Ephfullstack@users.noreply.github.com>
2023-10-13 11:28:06 +0100

    feat: start application

---
+++ app/config.ts
@@ -0,0 +1,9 @@
+export const config = {
+	mongoDB: {
+		debug: '',
+		atlasURL: '',
+		url: '',
+		dbLocalURL: 'mongodb://localhost:27017/whatsapp',
+		testDbURL: 'mongodb://localhost:27017/test-whatsapp',
+	},
+};
